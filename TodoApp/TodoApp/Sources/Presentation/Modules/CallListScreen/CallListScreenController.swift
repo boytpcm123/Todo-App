09c43eef47685b6dc58f5792d8a6392eb9f9a545
Thong Nguyen <thong.nguyen@hdwebsoft.co>
2022-03-19 23:14:23 +0700

    - Add CallListCell - Add tableView CallList

--- TodoApp/TodoApp/Sources/Presentation/Modules/CallListScreen/CallListScreenController.swift
+++ TodoApp/TodoApp/Sources/Presentation/Modules/CallListScreen/CallListScreenController.swift
@@ -21,11 +21,40 @@ class CallListScreenController: BaseViewController {
         }
     }
     
+    @IBOutlet private weak var callListTableView: UITableView! {
+        didSet {
+            self.callListTableView.registerReusedCell(cellNib: CallListCell.self, bundle: nil)
+            self.callListTableView.delegate = self
+            self.callListTableView.dataSource = self
+            self.callListTableView.tableFooterView = UIView()
+        }
+    }
+    
     // MARK: - PROPERTIES
     private var viewModel = HomeScreenViewModel()
     private let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+extension CallListScreenController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusable(cellNib: CallListCell.self, indexPath: indexPath) else {
+            return UITableViewCell()
+        }
+        return cell
+    }
+}
+
+extension CallListScreenController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("did select cell")
     }
 }
---
+++ TodoApp/TodoApp/Sources/Presentation/Views/CallListCell/CallListCell.swift
@@ -0,0 +1,31 @@
+//
+//  CallListCell.swift
+//  TodoApp
+//
+//  Created by hungdat1234 on 3/19/22.
+//
+
+import UIKit
+
+class CallListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+    
+//    override func configure<T>(data: T) {
+//        
+//    }
+    
+}
