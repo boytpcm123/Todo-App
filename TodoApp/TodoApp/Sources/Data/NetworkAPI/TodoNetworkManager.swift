2dd25a747c0643dd45b051cdd00f1068fe3d4cd4
Thong Nguyen <thong.nguyen@hdwebsoft.co>
2022-03-20 19:34:08 +0700

    - Quick, Nimble, RxNimble, RxSwift for testing

--- TodoApp/TodoApp/Sources/Data/NetworkAPI/TodoNetworkManager.swift
+++ TodoApp/TodoApp/Sources/Data/NetworkAPI/TodoNetworkManager.swift
@@ -16,7 +16,7 @@ struct TodoNetworkManager {
     
     private init() {
         let plugin: PluginType = NetworkLoggerPlugin(configuration: .init(logOptions: .verbose))
-        provider = MoyaProvider<TodoService>(plugins: [])
+        provider = MoyaProvider<TodoService>(plugins: [plugin])
     }
 }
 
